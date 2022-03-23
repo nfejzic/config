2df6f0eb3a76b79b2bb2d0fab00d755a89772837
Nadir Fejzic <nadirfejzo@gmail.com>
2022-03-23 13:50:13 +0100

    feat: add wezterm configuration

---
+++ wezterm/wezterm.lua
@@ -0,0 +1,32 @@
+local wezterm = require("wezterm")
+
+local gruvbox_dark_hard = wezterm.get_builtin_color_schemes()["Gruvbox Dark"]
+gruvbox_dark_hard.background = "#1d2021"
+
+-- this is a simple comment to check how does this font look like
+return {
+    -- font = wezterm.font("FiraCode Nerd Font"),
+    font = wezterm.font("Fira Code"),
+    -- font = wezterm.font("Noto Sans Mono"),
+    -- font = wezterm.font("Source Code Pro"),
+    font_size = 12,
+    freetype_load_target = "Light",
+    
+    color_schemes = {
+        ["Gruvbox Dark Hard"] = gruvbox_dark_hard
+    },
+    color_scheme = "Gruvbox Dark Hard",
+
+    hide_tab_bar_if_only_one_tab = true,
+
+    window_padding = {
+        left = "0.5cell",
+        right = "0.5cell",
+        top = "0.0cell",
+        bottom = 0,
+    },
+    window_decorations = "NONE",
+
+    initial_rows = 200,
+    initial_cols = 400,
+}
