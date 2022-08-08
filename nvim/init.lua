817c968a08210e7fcc096add8a4568292dbeed04
Nadir Fejzic <nadirfejzo@gmail.com>
2022-08-08 15:14:19 +0200

    chore(nvim): update config to taste

--- nvim/init.lua
+++ nvim/init.lua
@@ -9,9 +9,7 @@ vim.o.relativenumber = true
 
 require('plugins')
 require('config_lsp')
-
--- lsp-lightbulb
--- vim.cmd [[autocmd CursorHold,CursorHoldI * lua require'nvim-lightbulb'.update_lightbulb()]]
+require('dap_config')
 
 vim.cmd [[
   set nofoldenable
@@ -22,6 +20,7 @@ vim.cmd [[
     autocmd BufWinEnter *.php silent! :set syntax=php
     autocmd BufWinEnter *.html5 silent! :IndentBlanklineEnable
     autocmd BufWinEnter *.scss silent! :set syntax=scss
+    autocmd BufWinEnter *.vue silent! :set shiftwidth=2
   augroup end
 ]]
 
@@ -52,16 +51,27 @@ vim.o.smartcase = true
 
 --Decrease update time
 vim.o.updatetime = 250
-vim.o.signcolumn = 'yes:3'
+
+vim.o.signcolumn = 'yes:2'
+
 vim.o.cmdheight = 2
 vim.o.showmode = false
 
 --Colorscheme gruvbox dark hard (original, not material)
 vim.o.termguicolors = true
-vim.o.background = 'dark'
+-- vim.o.background = 'light'
 -- vim.cmd('colorscheme gruvbox-material')
 vim.g.gruvbox_contrast_dark = 'hard'
-vim.cmd([[colorscheme gruvbox]])
+
+require('catppuccin').setup()
+
+vim.g.tokyonight_style = "night"
+vim.g.catppuccin_flavour = "mocha"
+vim.cmd([[colorscheme catppuccin]])
+-- vim.cmd([[colorscheme solarized]])
+-- vim.cmd([[colorscheme gruvbox]])
+-- vim.cmd([[colorscheme tokyonight]])
+-- vim.cmd([[colorscheme nightfox]])
 
 -- Set completeopt to have a better completion experience
 vim.o.completeopt = 'menu,menuone,noselect,noinsert'
@@ -77,20 +87,23 @@ require('lualine').setup {
   options = {
     icons_enabled = true,
     theme = 'auto',
-    component_separators = { left = '', right = ''},
-    section_separators = { left = '', right = ''},
+    component_separators = { left = '', right = '' },
+    section_separators = { left = '', right = '' },
     -- component_separators = { left = '', right = ''},
     -- section_separators = { left = '', right = ''},
   },
   sections = {
-    lualine_a = {'mode'},
-    lualine_b = {'diff', 'branch'},
-    lualine_c = {'filename'},
-    lualine_x = {'encoding', 'fileformat', 'filetype'},
-    lualine_y = {'progress'},
-    lualine_z = {'location'}
+    lualine_a = { 'mode' },
+    lualine_b = { 'diff', 'branch' },
+    lualine_c = { 'filename' },
+    lualine_x = { 'encoding', 'fileformat', 'filetype' },
+    lualine_y = { 'progress' },
+    lualine_z = { 'location' }
   },
 }
+
+-- global statusline
+vim.o.laststatus = 3
 
 --Enable Comment.nvim
 require('Comment').setup()
@@ -117,18 +130,7 @@ vim.cmd [[
 -- vim.g.indent_blankline_filetype_exclude = { 'help', 'packer' }
 -- vim.g.indent_blankline_buftype_exclude = { 'terminal', 'nofile' }
 -- vim.g.indent_blankline_show_trailing_blankline_indent = false
-vim.g.indent_blankline_filetype = {'vim', 'NvimTree', 'html', 'php'}
-
--- Gitsigns
-require('gitsigns').setup {
-  signs = {
-    -- add = { text = '+' },
-    -- change = { text = '~' },
-    -- delete = { text = '_' },
-    -- topdelete = { text = '‾' },
-    -- changedelete = { text = '~' },
-  },
-}
+vim.g.indent_blankline_filetype = { 'vim', 'NvimTree', 'html', 'php' }
 
 -- Telescope
 require('telescope').setup {
@@ -138,12 +140,31 @@ require('telescope').setup {
     },
     mappings = {
       i = {
-        ['<C-u>'] = false,
-        ['<C-d>'] = false,
+        -- ['<C-u>'] = false,
+        -- ['<C-d>'] = false,
       },
     },
+    layout_strategy = 'vertical',
+    -- vimgrep_arguments = {
+    --   'rg',
+    --   '--color=never',
+    --   '--no-heading',
+    --   '--with-filename',
+    --   '--line-number',
+    --   '--column',
+    --   '--smart-case',
+    --   '--hidden',
+    -- }
   },
+  extensions = {
+    ['ui-select'] = {
+      require('telescope.themes').get_cursor()
+    }
+  }
 }
+
+-- Use Telescope for LSP Code Actions
+require('telescope').load_extension('ui-select')
 
 -- Enable telescope fzf native
 require('telescope').load_extension 'fzf'
@@ -155,7 +176,7 @@ require('nvim-treesitter.configs').setup {
   highlight = {
     enable = true, -- false will disable the whole extension
     disable = {
-        "html", "html5",
+      "html", "html5"
     },
     custom_captures = {
       -- with custom nfejzic/gruvbox theme
@@ -171,7 +192,7 @@ require('nvim-treesitter.configs').setup {
   },
   incremental_selection = {
     enable = false,
-    disable = { "c" },
+    disable = { "c", "vue" },
     keymaps = {
       init_selection = 'gnn',
       node_incremental = 'grn',
@@ -180,11 +201,11 @@ require('nvim-treesitter.configs').setup {
     },
   },
   indent = {
-    enable = false,
-    disable = { "c", "html5", "html" }
+    enable = true,
+    disable = { "c", "html5", "html", "vue" }
   },
   context_commentstring = {
-    enable = false
+    enable = true
   },
   textobjects = {
     select = {
@@ -230,13 +251,11 @@ vim.api.nvim_set_keymap('n', '<leader>lj', '<cmd>lua vim.diagnostic.goto_next()<
 vim.api.nvim_set_keymap('n', '<leader>q', '<cmd>lua vim.diagnostic.setloclist()<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<leader>q', '<cmd>lua vim.diagnostic.setloclist()<CR>', { noremap = true, silent = true })
 
-
 -- Format on save
 vim.cmd [[
   augroup Format_on_save
     autocmd!
-    autocmd BufWritePre * lua vim.lsp.buf.formatting_seq_sync()
-    " autocmd BufWritePre *.*css,*.less,*.html,*.ts,*.vue :Prettier<CR>
+    autocmd BufWritePre * lua vim.lsp.buf.formatting_sync()
   augroup end
 ]]
 
@@ -247,23 +266,18 @@ vim.api.nvim_set_keymap('n', '<leader>bC', '<cmd>BufferClose!<CR>', { noremap = 
 vim.api.nvim_set_keymap('n', '<C-l>', '<cmd>BufferNext<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<C-h>', '<cmd>BufferPrevious<CR>', { noremap = true, silent = true })
 
-require('nvim-autopairs').setup{}
-require('project_nvim').setup{}
+require('nvim-autopairs').setup {}
+require('project_nvim').setup {}
 
-vim.g.nvim_tree_respect_buf_cwd = 0
-require('nvim-tree').setup{
-  update_cwd = true,
+require('nvim-tree').setup {
+  sync_root_with_cwd = true,
   update_focused_file = {
     enable = true,
-    update_cwd = true,
+    update_root = true,
   },
   view = {
     side = 'left',
-    auto_resize = true,
-    hide_root_folder = false,
-    width = 30,
   },
-  quit_on_open = 0
 }
 
 vim.api.nvim_set_keymap('n', '<leader>ft', '<cmd>NvimTreeToggle<CR>', { noremap = true, silent = true })
@@ -276,10 +290,13 @@ wk.register({
   ['<leader>'] = {
     f = {
       name = "Find/File",
-      f = { "<cmd>lua require('telescope.builtin').find_files({ find_command = { 'rg', '--files', '--hidden', '--follow', '--ignore-file', '.gitignore' } })<CR>",  "Find file" },
-      b = { '<cmd>lua require("telescope.builtin").current_buffer_fuzzy_find({ find_command = "rg" })<CR>',  "Fuzzy search in buffer" },
-      t = { "<cmd>NvimTreeToggle<CR>",  "Open File Tree" },
-      p = { "<cmd>Telescope projects<CR>", "Recent projects" }
+      f = { "<cmd>lua require('telescope.builtin').find_files({ find_command = { 'rg', '--files', '--hidden', '--follow', '--ignore-file', '.gitignore' } })<CR>",
+        "Find file" },
+      b = { '<cmd>lua require("telescope.builtin").current_buffer_fuzzy_find({ find_command = "rg" })<CR>',
+        "Fuzzy search in buffer" },
+      t = { "<cmd>NvimTreeToggle<CR>", "Open File Tree" },
+      p = { "<cmd>Telescope projects<CR>", "Recent projects" },
+      d = { "<cmd>TodoTelescope<CR>", "Todo / Fixme etc." }
     },
     b = {
       name = "Buffer",
@@ -288,8 +305,10 @@ wk.register({
     },
     s = {
       name = "Search",
-      d = { "<cmd>lua require('telescope.builtin').grep_string({ find_command = 'rg' })<CR>",  'Grep string' },
-      p = { "<cmd>lua require('telescope.builtin').live_grep({ find_command = 'rg' })<CR>", 'Live grep string' },
+      d = { "<cmd>lua require('telescope.builtin').grep_string()<CR>", 'Grep string' },
+      l = { "<cmd>lua require('telescope.builtin').live_grep()<CR>", 'Live grep string' },
+      L = { "<cmd>lua require('telescope.builtin').live_grep({ additional_args = function () return {'--case-sensitive'} end })<CR>",
+        'Live grep string' },
       s = { "<cmd>lua require('telescope.builtin').treesitter()<CR>", 'Treesitter Document Symbols' },
     },
     k = {
@@ -300,10 +319,78 @@ wk.register({
     },
     ['<space>'] = { "<C-^>", 'Switch to previous buffer' },
     e = "Lsp Diagnostics popup",
-    ['?'] = { "<cmd>lua require('telescope.builtin').oldfiles()<CR>",  "Telescope - Old Files" }
+    ['?'] = { "<cmd>lua require('telescope.builtin').oldfiles()<CR>", "Telescope - Old Files" },
   }
 })
+
+-- Gitsigns
+local has_gitsigns, _ = pcall(require, 'gitsigns.nvim')
+
+if has_gitsigns then
+  require("gitsigns").setup {
+    signs = {
+      -- add = { text = '+' },
+      -- change = { text = '~' },
+      -- delete = { text = '_' },
+      -- topdelete = { text = '‾' },
+      -- changedelete = { text = '~' },
+    },
+    current_line_blame = true,
+    -- current_line_blame_formatter = '<author>, <author_time:%Y-%m-%d> - <summary>',
+  }
+
+  wk.register({
+    ['<leader>'] = {
+      g = {
+        name = 'Git',
+        j = { '<cmd>Gitsigns next_hunk<CR>', 'Next hunk' },
+        k = { '<cmd>Gitsigns prev_hunk<CR>', 'Previous hunk' },
+        p = { '<cmd>Gitsigns preview_hunk<CR>', 'Preview hunk' },
+        s = { '<cmd>Gitsigns stage_hunk<CR>', 'Stage hunk' },
+        u = { '<cmd>Gitsigns stage_hunk<CR>', 'Unstage hunk' },
+        r = { '<cmd>Gitsigns reset_hunk<CR>', 'Reset hunk' },
+        d = { '<cmd>Gitsigns diffthis<CR>', 'Diff this' },
+        q = { '<cmd>Gitsigns setqflist<CR>', 'Show changes in quickfix list' },
+      }
+    }
+  })
+end
+
+-- fugitive specific, also requires 'tpope/vim-fugitive'
+wk.register({
+  ['<leader>'] = {
+    g = {
+      name = 'Git',
+      g = { '<cmd>G<CR>', 'Git status' }
+    },
+    -- Git merge:
+    m = {
+      name = 'Merge',
+      h = { '<cmd>diffget //2<CR>', 'Use ours' },
+      l = { '<cmd>diffget //2<CR>', 'Use theirs' },
+    }
+  }
+})
+
+local has_trouble, trouble = pcall(require, "trouble")
+
+if has_trouble then
+  trouble.setup {}
+
+  wk.register({
+    t = {
+      name = "Trouble",
+      q = { '<cmd>TroubleToggle quickfix<CR>', 'Quickfix' },
+      l = { '<cmd>TroubleToggle loclist<CR>', 'Loclist' },
+      d = { '<cmd>TroubleToggle document_diagnostics<CR>', 'Document Diagnostics' },
+      w = { '<cmd>TroubleToggle workspace_diagnostics<CR>', 'Workspace Diagnostics' },
+      t = { '<cmd>TodoTrouble<CR>', 'TODO / FIXME etc.' },
+    }
+  })
+end
 
 vim.g.bufferline = {
   animation = false,
 }
+
+require('colorizer').setup()
