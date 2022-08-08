8b92ed3c842aea0e73841388a0901ed5fb9aca91
Nadir Fejzic <nadirfejzo@gmail.com>
2022-08-08 15:22:13 +0200

    feat(wezterm): add some config options and themes

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -6,26 +6,32 @@ gruvbox_dark_hard.background = "#1d2021"
 -- this is a simple comment to check how does this font look like
 return {
     -- font = wezterm.font("FiraCode Nerd Font"),
-    -- font = wezterm.font("Fira Code"),
     -- font = wezterm.font("Noto Sans Mono"),
     -- font = wezterm.font("Source Code Pro"),
     -- font = wezterm.font("Hack"),
-    font = wezterm.font("JetBrains Mono"),
-    font_size = 12,
+    font = wezterm.font("JetBrains Mono"), -- ->
+    -- font = wezterm.font("Cascadia Code"),
+    font_size = 11,
     freetype_load_target = "Light",
-    
-    -- l1gq@a
-    -- @some_thing
+
+    -- enable cursive italic form
+    -- harfbuzz_features = { "calt", "ss01" },
+
     color_schemes = {
         ["Gruvbox Dark Hard"] = gruvbox_dark_hard
     },
-    color_scheme = "Gruvbox Dark Hard",
+    -- color_scheme = "Gruvbox Dark Hard",
+    color_scheme = "Catppuccin",
+    -- color_scheme = "Gruvbox Light",
+    -- color_scheme = "Builtin Solarized Light",
+    -- color_scheme = "tokyonight",
+    -- color_scheme = "nightfox",
 
     hide_tab_bar_if_only_one_tab = true,
 
     window_padding = {
-        left = "0.5cell",
-        right = "0.5cell",
+        left = "0.0cell",
+        right = "0.0cell",
         top = "0.0cell",
         bottom = 0,
     },
@@ -33,4 +39,6 @@ return {
 
     initial_rows = 200,
     initial_cols = 400,
+
+    warn_about_missing_glyphs = false
 }
