4e7f2da7c17cb226b9ad526a73c6c25e5e2c7da1
Nadir Fejzic <nadirfejzo@gmail.com>
2022-08-09 15:50:42 +0200

    feat(nvim): disable tsserver formatting for typescript

--- nvim/lua/config_lsp.lua
+++ nvim/lua/config_lsp.lua
@@ -110,8 +110,8 @@ mason_lsp.setup_handlers {
   ["tsserver"] = function()
     lspconfig["tsserver"].setup {
       on_attach = function(client, bufnr)
-        client.server_capabilities.document_formatting = false
-        client.server_capabilities.document_range_formatting = false
+        client.resolved_capabilities.document_formatting = false
+        client.resolved_capabilities.document_range_formatting = false
 
         local ts_utils = require("nvim-lsp-ts-utils")
         ts_utils.setup({})
