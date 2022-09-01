f5c76cf823f7dcd0873edec53c1159874dc687e2
Nadir Fejzic <nadirfejzo@gmail.com>
2022-09-01 16:31:10 +0200

    chore(nvim): remove commitlint from null-ls config

--- nvim/lua/config_lsp.lua
+++ nvim/lua/config_lsp.lua
@@ -250,7 +250,6 @@ mason_lsp.setup_handlers {
 local null_ls = require('null-ls');
 null_ls.setup({
   sources = {
-    null_ls.builtins.diagnostics.commitlint,
     null_ls.builtins.code_actions.eslint_d,
     null_ls.builtins.formatting.prettierd,
     null_ls.builtins.formatting.eslint_d,
