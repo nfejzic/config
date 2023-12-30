2752c0f42b56ceb00981432a39ecb31805936962
Nadir Fejzic <nadirfejzo@gmail.com>
2023-12-30 15:14:39 +0100

    chore(wezterm): update config

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -5,29 +5,34 @@ local wezterm = require("wezterm")
 local mux = wezterm.mux
 
 wezterm.on("gui-startup", function(cmd)
-  local _, _, window = mux.spawn_window(cmd or {})
-  window:gui_window():maximize()
+	local _, _, window = mux.spawn_window(cmd or {})
+	window:gui_window():maximize()
 end)
 
 local theme = "Gruvbox dark, hard (base16)"
-theme = "Catppuccin Mocha"
+-- theme = "Catppuccin Mocha"
+theme = "Catppuccin Macchiato"
+-- theme = "rose-pine"
 -- theme = "Kanagawa (Gogh)"
+-- theme = "Mariana"
 local colors = wezterm.color.get_builtin_schemes()[theme]
 
-wezterm.on(
-  'format-tab-title',
-  tab_fns.format_tab_title(colors)
-)
+wezterm.on("format-tab-title", tab_fns.format_tab_title(colors))
 
-wezterm.on('update-status', function(window, _pane)
-  local workspace = window:active_workspace()
-  window:set_left_status(
-    wezterm.format({
-      { Attribute = { Intensity = 'Half' } },
-      { Background = { Color = colors.ansi[5] } },
-      { Foreground = { Color = colors.ansi[1] } },
-      { Text = ' [' .. workspace .. '] ' },
-    }))
+wezterm.on("update-status", function(window, _pane)
+	local workspace = window:active_workspace()
+
+	local foreground_color = colors.ansi[1]
+	if theme == "rose-pine" then
+		foreground_color = colors.brights[8]
+	end
+
+	window:set_left_status(wezterm.format({
+		{ Attribute = { Intensity = "Normal" } },
+		{ Background = { Color = colors.brights[5] } },
+		{ Foreground = { Color = foreground_color } },
+		{ Text = " [" .. workspace .. "] " },
+	}))
 end)
 
 -- This table will hold the configuration.
@@ -36,24 +41,37 @@ local config = {}
 -- In newer versions of wezterm, use the config_builder which will
 -- help provide clearer error messages
 if wezterm.config_builder then
-  config = wezterm.config_builder()
+	config = wezterm.config_builder()
 end
 
+config.font = wezterm.font("JetBrainsMono Nerd Font")
+config.font_size = 26
+
+-- config.font = wezterm.font("Consolas")
+-- config.font_size = 28
+-- config.line_height = 1.05
+
 -- ->
-config.font = wezterm.font("JetBrains Mono")
-config.font_size = 20
+-- config.font = wezterm.font("Monolisa")
+-- config.font_size = 24
+-- config.line_height = 1.19
+
+-- config.font = wezterm.font("Berkeley Mono")
+-- config.font_size = 26
+-- config.line_height = 1.1
+
 config.bold_brightens_ansi_colors = "BrightAndBold"
 
 config.color_scheme = theme
 
 config.window_padding = {
-  left = "0cell",
-  right = "0cell",
-  top = "0cell",
-  bottom = "0.0cell",
+	left = "0cell",
+	right = "0cell",
+	top = "0cell",
+	bottom = "0.0cell",
 }
 
--- window_decorations = "RESIZE",
+-- window_decorations = "RESIZE"
 config.warn_about_missing_glyphs = true
 config.hide_mouse_cursor_when_typing = false
 config.enable_scroll_bar = false
@@ -63,30 +81,9 @@ config.hide_tab_bar_if_only_one_tab = false
 config.use_fancy_tab_bar = false
 config.tab_bar_at_bottom = true
 config.show_new_tab_button_in_tab_bar = false
-config.window_frame = {
-  -- The font used in the tab bar.
-  -- Roboto Bold is the default; this font is bundled
-  -- with wezterm.
-  -- Whatever font is selected here, it will have the
-  -- main font setting appended to it to pick up any
-  -- fallback fonts you may have used there.
-  font = wezterm.font("JetBrains Mono"),
-
-  -- The size of the font in the tab bar.
-  -- Default to 10.0 on Windows but 12.0 on other systems
-  -- font_size = 18.0,
-
-  -- The overall background color of the tab bar when
-  -- the window is focused
-  active_titlebar_bg = colors.background,
-
-  -- The overall background color of the tab bar when
-  -- the window is not focused
-  inactive_titlebar_bg = colors.background,
-}
 
 config.colors = {
-  tab_bar = tab_fns.tab_bar_colors(colors),
+	tab_bar = tab_fns.tab_bar_colors(colors),
 }
 
 config.leader = keys.get_keybindings(wezterm).leader
