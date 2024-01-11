b7462047546963f14c69f59a6e837da3ead29a4e
Nadir Fejzic <nadirfejzo@gmail.com>
2024-01-11 10:37:38 +0100

    feat(wezterm): update keymaps in wezterm

--- wezterm/key_config.lua
+++ wezterm/key_config.lua
@@ -2,183 +2,182 @@ local M = {}
 -- timeout_milliseconds defaults to 1000 and can be omitted
 
 M.get_keybindings = function(wezterm)
-  local act = wezterm.action
-  return {
-    leader = {
-      key = " ",
-      mods = "ALT",
-      timeout_milliseconds = 1000,
-    },
-    keys = {
-      -- split pane vertically
-      {
-        key = "n",
-        mods = "LEADER",
-        action = act.SplitHorizontal({ domain = "CurrentPaneDomain" }),
-      },
+	local act = wezterm.action
+	return {
 
-      -- split pane horizontally
-      {
-        key = "d",
-        mods = "LEADER",
-        action = act.SplitVertical({ domain = "CurrentPaneDomain" }),
-      },
+		leader = {
+			key = " ",
+			mods = "ALT",
+			timeout_milliseconds = 1000,
+		},
 
-      -- split pane horizontally
-      {
-        key = "c",
-        mods = "LEADER",
-        action = act.CloseCurrentPane({ confirm = false }),
-      },
+		keys = {
+			{
+				key = "n",
+				mods = "LEADER",
+				action = act.SplitHorizontal({ domain = "CurrentPaneDomain" }),
+			},
 
-      -- split pane horizontally
-      {
-        key = "c",
-        mods = "LEADER|SHIFT",
-        action = act.CloseCurrentPane({ confirm = false }),
-      },
+			{
+				key = "d",
+				mods = "LEADER",
+				action = act.SplitVertical({ domain = "CurrentPaneDomain" }),
+			},
 
-      -- go to pane to the left of current pane
-      {
-        key = "h",
-        mods = "ALT",
-        action = act.ActivatePaneDirection("Left"),
-      },
+			{
+				key = "c",
+				mods = "LEADER",
+				action = act.CloseCurrentPane({ confirm = false }),
+			},
 
-      -- go to pane to the right of current pane
-      {
-        key = "l",
-        mods = "ALT",
-        action = act.ActivatePaneDirection("Right"),
-      },
+			-- go to pane to the left of current pane
+			{
+				key = "h",
+				mods = "ALT",
+				action = act.ActivatePaneDirection("Left"),
+			},
 
-      -- go to pane below current pane
-      {
-        key = "j",
-        mods = "ALT",
-        action = act.ActivatePaneDirection("Down"),
-      },
+			-- go to pane to the right of current pane
+			{
+				key = "l",
+				mods = "ALT",
+				action = act.ActivatePaneDirection("Right"),
+			},
 
-      -- go to pane above current pane
-      {
-        key = "k",
-        mods = "ALT",
-        action = act.ActivatePaneDirection("Up"),
-      },
+			-- go to pane below current pane
+			{
+				key = "j",
+				mods = "ALT",
+				action = act.ActivatePaneDirection("Down"),
+			},
 
-      -- pane resizing
-      {
-        key = "H",
-        mods = "ALT|SHIFT",
-        action = act.AdjustPaneSize({ "Left", 5 }),
-      },
+			-- go to pane above current pane
+			{
+				key = "k",
+				mods = "ALT",
+				action = act.ActivatePaneDirection("Up"),
+			},
 
-      {
-        key = "J",
-        mods = "ALT|SHIFT",
-        action = act.AdjustPaneSize({ "Down", 5 }),
-      },
+			-- pane resizing
+			{
+				key = "H",
+				mods = "ALT|SHIFT",
+				action = act.AdjustPaneSize({ "Left", 5 }),
+			},
 
-      {
-        key = "K",
-        mods = "ALT|SHIFT",
-        action = act.AdjustPaneSize({ "Up", 5 }),
-      },
+			{
+				key = "J",
+				mods = "ALT|SHIFT",
+				action = act.AdjustPaneSize({ "Down", 5 }),
+			},
 
-      {
-        key = "L",
-        mods = "ALT|SHIFT",
-        action = act.AdjustPaneSize({ "Right", 5 }),
-      },
+			{
+				key = "K",
+				mods = "ALT|SHIFT",
+				action = act.AdjustPaneSize({ "Up", 5 }),
+			},
 
-      -- create new tab
-      {
-        key = "t",
-        mods = "LEADER",
-        action = act.SpawnTab("CurrentPaneDomain"),
-      },
+			{
+				key = "L",
+				mods = "ALT|SHIFT",
+				action = act.AdjustPaneSize({ "Right", 5 }),
+			},
 
-      -- go to next tab
-      {
-        key = "n",
-        mods = "ALT",
-        action = act.ActivateTabRelative(1),
-      },
+			-- create new tab
+			{
+				key = "t",
+				mods = "LEADER",
+				action = act.SpawnTab("CurrentPaneDomain"),
+			},
 
-      -- go to previous tab
-      {
-        key = "p",
-        mods = "ALT",
-        action = act.ActivateTabRelative(-1),
-      },
+			-- go to next tab
+			{
+				key = "n",
+				mods = "ALT",
+				action = act.ActivateTabRelative(1),
+			},
 
-      -- Prompt for a name to use for a new workspace and switch to it.
-      {
-        key = "n",
-        mods = "SUPER",
-        action = wezterm.action_callback(function(_win, _p, _l)
-          local active_workspaces = wezterm.mux.get_workspace_names()
+			-- go to previous tab
+			{
+				key = "p",
+				mods = "ALT",
+				action = act.ActivateTabRelative(-1),
+			},
 
-          local t = ""
+			-- Prompt for a name to use for a new workspace and switch to it.
+			{
+				key = "n",
+				mods = "SUPER",
+				action = wezterm.action_callback(function(_win, _p, _l)
+					local active_workspaces = wezterm.mux.get_workspace_names()
 
-          if #active_workspaces > 1 then
-            t = t .. "Currently active workspaces: \n"
-            for i, n in ipairs(active_workspaces) do
-              t = t .. i .. ": " .. n .. "\n"
-            end
-            t = t .. "\n"
-          end
+					local t = ""
 
-          t = t .. "Enter name for new (or existing) workspace"
+					if #active_workspaces > 1 then
+						t = t .. "Currently active workspaces: \n"
+						for i, n in ipairs(active_workspaces) do
+							t = t .. i .. ": " .. n .. "\n"
+						end
+						t = t .. "\n"
+					end
 
-          _win:perform_action(
-            act.PromptInputLine({
-              description = wezterm.format({
-                { Attribute = { Intensity = "Bold" } },
-                { Foreground = { AnsiColor = "Fuchsia" } },
-                { Text = t },
-              }),
-              action = wezterm.action_callback(function(window, pane, line)
-                -- line will be `nil` if they hit escape without entering anything
-                -- An empty string if they just hit enter
-                -- Or the actual line of text they wrote
-                if line then
-                  window:perform_action(
-                    act.SwitchToWorkspace({
-                      name = line,
-                    }),
-                    pane
-                  )
-                end
-              end),
-            }),
-            _p
-          )
-        end),
-      },
+					t = t .. "Enter name for new (or existing) workspace"
 
-      {
-        key = "s",
-        mods = "LEADER",
-        action = act.ShowLauncherArgs({ flags = "FUZZY|WORKSPACES" }),
-      },
+					_win:perform_action(
+						act.PromptInputLine({
+							description = wezterm.format({
+								{ Attribute = { Intensity = "Bold" } },
+								{ Foreground = { AnsiColor = "Fuchsia" } },
+								{ Text = t },
+							}),
+							action = wezterm.action_callback(function(window, pane, line)
+								-- line will be `nil` if they hit escape without entering anything
+								-- An empty string if they just hit enter
+								-- Or the actual line of text they wrote
+								if line then
+									window:perform_action(
+										act.SwitchToWorkspace({
+											name = line,
+										}),
+										pane
+									)
+								end
+							end),
+						}),
+						_p
+					)
+				end),
+			},
 
-      {
-        key = "l",
-        mods = "LEADER",
-        action = act.ShowLauncherArgs({
-          flags = "FUZZY|TABS|DOMAINS|KEY_ASSIGNMENTS|WORKSPACES|COMMANDS|LAUNCH_MENU_ITEMS",
-        }),
-      },
+			{
+				key = "s",
+				mods = "LEADER",
+				action = act.ShowLauncherArgs({ flags = "FUZZY|WORKSPACES" }),
+			},
 
-      -- Send "CTRL-A" to the terminal when pressing CTRL-A, CTRL-A
-      {
-        key = "a",
-        mods = "LEADER|CTRL",
-        action = act.SendKey({ key = "a", mods = "CTRL" }),
-      },
-    },
-  }
+			{
+				key = "l",
+				mods = "LEADER",
+				action = act.ShowLauncherArgs({
+					flags = "FUZZY|TABS|DOMAINS|KEY_ASSIGNMENTS|WORKSPACES|COMMANDS|LAUNCH_MENU_ITEMS",
+				}),
+			},
+
+			-- Send "CTRL-A" to the terminal when pressing CTRL-A, CTRL-A
+			{
+				key = "a",
+				mods = "LEADER|CTRL",
+				action = act.SendKey({ key = "a", mods = "CTRL" }),
+			},
+
+			-- enter the quickselection mode
+			{
+				key = "v",
+				mods = "LEADER",
+				action = act.QuickSelect,
+			},
+		},
+	}
 end
 
 return M
