74ac85054888e99c42f4e393a1738d6ed1cf84e2
Nadir Fejzic <nadirfejzo@gmail.com>
2024-02-18 00:14:35 +0100

    fix(wezterm): use correct dpi, set font size accordingly

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -44,21 +44,26 @@ if wezterm.config_builder then
 	config = wezterm.config_builder()
 end
 
-config.font = wezterm.font("JetBrainsMono Nerd Font")
-config.font_size = 26
+config.front_end = "WebGpu"
 
--- config.font = wezterm.font("Consolas")
--- config.font_size = 28
--- config.line_height = 1.05
+if wezterm.hostname() == "zenith" then
+	-- Mac Mini, macos uses default DPI 72 for non HiDPI screens.
+	-- However, my LG QHD Monitor has DPI of 108. Set accordingly, otherwise
+	-- fonts get too small
+	config.dpi = 108
+end
+
+-- font test
+local _ = "gq 1Il O0 0123456789"
+
+-- long time favorite font:
+config.font = wezterm.font("JetBrainsMono Nerd Font")
+config.font_size = 12.5
 
 -- ->
 -- config.font = wezterm.font("Monolisa")
--- config.font_size = 24
--- config.line_height = 1.19
-
--- config.font = wezterm.font("Berkeley Mono")
--- config.font_size = 26
--- config.line_height = 1.1
+-- config.font_size = 10
+-- config.line_height = 1.18
 
 config.bold_brightens_ansi_colors = "BrightAndBold"
 
