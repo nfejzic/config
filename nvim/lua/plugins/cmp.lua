7cc383bd49d0496ed2b0e84862018c04419509ff
Nadir Fejzic <nadirfejzo@gmail.com>
2024-02-20 21:32:25 +0100

    feat(nvim): add copilot icon to nvim-cmp

--- nvim/lua/plugins/cmp.lua
+++ nvim/lua/plugins/cmp.lua
@@ -39,7 +39,11 @@ return {
 					-- format = lspkind.cmp_format({ mode = 'symbol' })
 					fields = { "kind", "abbr", "menu" },
 					format = function(entry, vim_item)
-						local kind = lspkind.cmp_format({ mode = "symbol_text", maxwidth = 50 })(entry, vim_item)
+						local kind = lspkind.cmp_format({
+							mode = "symbol_text",
+							maxwidth = 50,
+							symbol_map = { Copilot = "" },
+						})(entry, vim_item)
 						local strings = vim.split(kind.kind, "%s", { trimempty = true })
 						kind.kind = " " .. (strings[1] or "") .. " "
 						kind.menu = "    (" .. (strings[2] or "") .. ")"
