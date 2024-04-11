aecffb922c665e7a14fd40c897dc4a612e2ba0d3
Nadir Fejzic <nadirfejzo@gmail.com>
2024-04-11 13:58:34 +0200

    chore(nvim): add code snippet for polling env var

--- nvim/lua/user/opts.lua
+++ nvim/lua/user/opts.lua
@@ -182,3 +182,17 @@ vim.api.nvim_create_autocmd("BufReadPost", {
 		end
 	end,
 })
+
+-- local utils = require("user.utils")
+--
+-- local interval = utils.setInterval(10000, function()
+-- 	print("Polling env variables")
+-- 	local theme = os.getenv("CLI_THEME")
+-- 	print("CLI_THEME=" .. theme)
+-- end)
+--
+-- vim.api.nvim_create_autocmd("VimLeavePre", {
+-- 	callback = function()
+-- 		utils.clearInterval(interval)
+-- 	end,
+-- })
