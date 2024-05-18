92c4c0c0283c05cbf08b84be56a585b05822454a
Nadir Fejzic <nadirfejzo@gmail.com>
2024-05-19 00:53:20 +0200

    fix(nvim): remove obsolete telescope workaround

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -51,18 +51,6 @@ return {
 			-- Enable extension for browsing through git history of a file
 			require("telescope").load_extension("git_file_history")
 
-			-- NOTE: Currently (on neovim nightly) there's a problem with telescope where
-			-- fin_files opens the file in insert mode. This is a workaround for that
-			-- problem
-			vim.api.nvim_create_autocmd("WinLeave", {
-				callback = function()
-					-- Prevent entering buffers in insert mode.
-					if vim.bo.ft == "TelescopePrompt" and vim.fn.mode() == "i" then
-						vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Esc>", true, false, true), "i", false)
-					end
-				end,
-			})
-
 			local telescope = require("telescope")
 			local t_builtin = require("telescope.builtin")
 
