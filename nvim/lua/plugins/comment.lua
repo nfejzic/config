98691ddf943b1528646aa4e7657a217576e3b400
Nadir Fejzic <nadirfejzo@gmail.com>
2024-05-19 01:49:31 +0200

    feat(nvim): experimentally use built-in vim-commentary

--- nvim/lua/plugins/comment.lua
+++ nvim/lua/plugins/comment.lua
@@ -1,16 +1,20 @@
 return {
-	-- "gc" to comment visual regions/lines
-	{
-		"numToStr/Comment.nvim",
-
-		dependencies = { "JoosepAlviste/nvim-ts-context-commentstring" },
-
-		config = function()
-			require("Comment").setup({
-				pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
-			})
-		end,
-
-		event = { "BufEnter" },
-	},
+	-- NOTE: this is now built-in to Neovim (from v0.10.0)
+	--		 however, it does not support block comments, and it's not clear if
+	--		 works that well for all use cases. If some commenting does not work
+	--		 as wished, maybe re-enable the plugin...
+	-- 	-- "gc" to comment visual regions/lines
+	-- 	{
+	-- 		"numToStr/Comment.nvim",
+	--
+	-- 		dependencies = { "JoosepAlviste/nvim-ts-context-commentstring" },
+	--
+	-- 		config = function()
+	-- 			require("Comment").setup({
+	-- 				pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
+	-- 			})
+	-- 		end,
+	--
+	-- 		event = { "BufEnter" },
+	-- 	},
 }
