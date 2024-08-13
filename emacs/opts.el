d669c22a739432221102be8bf84aaa3bef25cb20
Nadir Fejzic <nadir@notfloor.com>
2024-08-13 23:14:43 +0200

    feat: add some emacs config

---
+++ emacs/opts.el
@@ -0,0 +1,19 @@
+(evil-mode 1)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("476fe2180054c68863477bd43617e17bb4d10b75e44f6efed146cc55d7b28809" default)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+; load custom themes
+(push (concat user-emacs-directory "themes/") custom-theme-load-path)
+
+(load-theme 'kanagawa)
