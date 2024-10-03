1dab0d4c8f04c17664d245e80baf17ab836836ee
Nadir Fejzic <n.fejzic@commend.com>
2024-10-03 09:50:14 +0200

    feat(nvim): add keybind to expand item in qflist

--- nvim/lua/plugins/quicker.lua
+++ nvim/lua/plugins/quicker.lua
@@ -1,4 +1,12 @@
 return {
 	"stevearc/quicker.nvim",
-	config = true,
+	config = function()
+		local quicker = require('quicker')
+		quicker.setup({
+			keys = {
+				{ ">", require('quicker').expand,   desc = "Expand quickfix content" },
+				{ "<", require('quicker').collapse, desc = "Expand quickfix content" },
+			}
+		})
+	end
 }
