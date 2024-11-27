51c0d223e14c55cbc4ff4fa4dfdc2bc08059e92f
Nadir Fejzic <nadir@notfloor.com>
2024-11-27 23:42:08 +0100

    feat(fish): make `gbd` abbreviation a little safer
    Previously, I just looked for branches that have "gone" somewhere
    associated with them. More precisely, git shows ": gone]" for branches
    that are deleted on remote. So this change makes sure we check for a
    little more specific wording, so that we don't accidentaly delete
    branches that we still need.

--- fish/user/abbr.fish
+++ fish/user/abbr.fish
@@ -68,7 +68,7 @@ if type -q git
     abbr -a gpr 'git pull --prune'
     abbr -a gpp 'git push'
     abbr -a gb 'git branch'
-    abbr -a gbd 'git branch -vv | rg gone | awk \'{print $1}\' | xargs -p -I _ git branch -D _'
+    abbr -a gbd 'git branch -vv | rg ": gone]" | awk \'{print $1}\' | xargs -p -I _ git branch -D _'
     abbr -a gw 'git worktree'
     abbr -a gwa 'git worktree add' # checkout branch (directory) in worktree
     abbr -a gwr 'git worktree remove' # remove branch (directory) in worktree
