376c660f6525d8141ae723445cd5d83e1875548f
Nadir Fejzic <nadir@notfloor.com>
2024-12-23 15:27:03 +0100

    chore(fish): load brew first, then rest of config

--- fish/user/init.fish
+++ fish/user/init.fish
@@ -1,6 +1,3 @@
-starship init fish | source
-set fish_greeting
-
 function import -a path
     set pwd (dirname (status -f))
     source "$pwd/$path"
@@ -8,10 +5,10 @@ end
 
 # ADD IMPORTS HERE
 
+import "./setup_brew.fish"
 import "./utils.fish"
 import "./theme.fish"
 import "./abbr.fish"
-import "./setup_brew.fish"
 import "./editor.fish"
 import "./env.fish"
 import "./rectangle.fish"
@@ -24,3 +21,6 @@ set -gx GPG_TTY (tty)
 function reload_config
     source ~/.config/fish/config.fish
 end
+
+starship init fish | source
+set fish_greeting
