ab8654873ffdb750657ec8d7d47f039bd1c6df92
Nadir Fejzic <nadir@notfloor.com>
2024-12-23 15:27:59 +0100

    feat(fish): add function for removing values from PATH

--- fish/user/utils.fish
+++ fish/user/utils.fish
@@ -83,3 +83,18 @@ function log
         echo ": $argv"
     end
 end
+
+function remove_from_path
+    set -l value $argv[1]
+    echo "Trying to remove $value from PATH, fish_user_path"
+    
+    if set -l index (contains -i $value $PATH)
+        echo "Removing \$PATH[$index] = $value"
+        set -e PATH[$index]
+    end
+
+    if set -l index (contains -i $value $fish_user_paths)
+        echo "Removing \$fish_user_paths[$index] = $value"
+        set -e fish_user_paths[$index]
+    end
+end
