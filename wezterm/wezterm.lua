22d00578e32945877435f31e1707f081bc583c9c
Nadir Fejzic <nadir@notfloor.com>
2025-01-18 18:55:40 +0100

    refactor(wezterm): simplify main config file

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,12 +1,9 @@
 local wezterm = require("wezterm")
 
-local config = {}
-
 -- In newer versions of wezterm, use the config_builder which will
 -- help provide clearer error messages
-if wezterm.config_builder then
-	config = wezterm.config_builder()
-end
+-- NOTE: Old versions without `config_builder` are not supported.
+local config = wezterm.config_builder()
 
 local library = require("lib.init")
 library.setup(wezterm, config)
