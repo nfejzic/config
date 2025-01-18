3ab7e4933cc1126fb66f7b85061dd00ad23bf442
Nadir Fejzic <nadir@notfloor.com>
2025-01-18 18:54:21 +0100

    chore(nvim): adjust colorize settings

--- nvim/lua/plugins/colorize.lua
+++ nvim/lua/plugins/colorize.lua
@@ -4,19 +4,26 @@ return {
 		lazy = false,
 		dev = true,
 		priority = 1000,
-		opts = {
-			compile = false, -- enable compiling the colorscheme
-			undercurl = true, -- enable undercurls
-			commentStyle = { italic = false },
-			functionStyle = {},
-			keywordStyle = { italic = false, bold = false },
-			statementStyle = { bold = false },
-			typeStyle = { italic = false, bold = false },
-			transparent = false, -- do not set background color
-			dimInactive = false, -- dim inactive window `:h hl-NormalNC`
-			terminalColors = true, -- define vim.g.terminal_color_{0,17}
-			theme = "kanagawa", -- Load "kanagawa" theme when 'background' option is not set
-			semantic_highlighting = "minimal",
-		}
+		lualine_bold = true,
+		config = function()
+			local colorize = require('colorize')
+
+			vim.g.colorize_lualine_bold = true
+
+			colorize.setup({
+				compile = false, -- enable compiling the colorscheme
+				undercurl = true, -- enable undercurls
+				commentStyle = { italic = false },
+				functionStyle = {},
+				keywordStyle = { italic = false, bold = false },
+				statementStyle = { bold = false },
+				typeStyle = { italic = false, bold = false },
+				transparent = false, -- do not set background color
+				dimInactive = false, -- dim inactive window `:h hl-NormalNC`
+				terminalColors = true, -- define vim.g.terminal_color_{0,17}
+				theme = "kanagawa", -- Load "kanagawa" theme when 'background' option is not set
+				semantic_highlighting = "minimal",
+			})
+		end
 	}
 }
