abfe8acb9c62d1d37f6e85c4f2414384466e4138
Nadir Fejzić <nadir.fejzic@helsing.ai>
2025-02-11 13:05:38 +0100

    feat(nvim): add formatter for yaml

--- nvim/lua/user/formatting.lua
+++ nvim/lua/user/formatting.lua
@@ -12,6 +12,7 @@ conform.setup({
 		vue = { "prettierd", "prettier", stop_after_first = true },
 		html = { "prettierd", "prettier", stop_after_first = true },
 		markdown = { "prettierd", "prettier", stop_after_first = true },
+		yaml = { "yamlfmt" },
 		rust = {},
 		c = {},
 		go = { "gofumpt" },
