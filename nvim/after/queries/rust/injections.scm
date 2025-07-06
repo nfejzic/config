228a8d2ffeecf45f4f1259fdb6e04e0e94d4849b
Nadir Fejzić <nadir@notfloor.com>
2025-07-06 12:18:20 +0200

    feat(nvim): add SQL injections to Rust

--- nvim/after/queries/rust/injections.scm
+++ nvim/after/queries/rust/injections.scm
@@ -1,19 +1,7 @@
 ; extends
 
-(call_expression
-  function: (_
-    path: _
-    name: (identifier) @_query_ident (#any-of? @_query_ident "query" "query_as" "query_with" "query_as_with")
-  )
-  arguments: (arguments
-            (string_literal) @sql (#offset! @sql 0 1 0 -1))
-)
-
-(call_expression
-  function: (_
-    path: _
-    name: (identifier) @_query_ident (#any-of? @_query_ident "query" "query_as" "query_with" "query_as_with")
-  )
-  arguments: (arguments
-               (raw_string_literal) @sql (#offset! @sql 0 3 0 -2))
+(
+    (string_content) @injection.content
+    (#match? @injection.content "^[ \t\n]*(SELECT|INSERT|UPDATE|DELETE|WITH|USE|DROP|ALTER|SET).*")
+    (#set! injection.language "sql")
 )
