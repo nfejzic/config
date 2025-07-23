3f308b1400ec1036dd53cc41feef4301d05e8f8d
Nadir Fejzic <nadir@notfloor.com>
2025-07-23 22:48:31 +0200

    feat(emacs): use emacs-kick config

---
+++ emacs/ek-reinstall.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+clear
+echo ">>> (re)Installing Emacs Kick ..."
+sleep 2
+
+echo ">>> Deleting packages, grammars and native compilation cache ..."
+rm -rf eln-cache/ elpa/ tree-sitter/ straight/
+sleep 2
+
+echo ">>> Starting Emacs and auto-package fetching/installing ..."
+sleep 2
+emacs --init-dir="./" -nw --eval="(ek/first-install)"
