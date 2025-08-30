3d2e89ba27f7776882c4f8286e059e5e910ae855
Nadir Fejzić <nadir@notfloor.com>
2025-08-30 11:12:15 +0200

    feat(fish): set cursor style based on universal variable

--- fish/user/env.fish
+++ fish/user/env.fish
@@ -3,17 +3,19 @@ set -gx EMAIL 'nadir@notfloor.com'
 set -gx VOLTA_HOME "$HOME/.volta"
 set -gx PATH "$VOLTA_HOME/bin" $PATH
 
-set -gx fish_cursor_default block
-set -gx fish_cursor_insert line blink
-set -gx fish_cursor_external line blink
-set -gx fish_cursor_replace_one underscore
-set -gx fish_cursor_visual block
-
-# set -gx fish_cursor_default block
-# set -gx fish_cursor_insert block blink
-# set -gx fish_cursor_external block blink
-# set -gx fish_cursor_replace_one underscore
-# set -gx fish_cursor_visual block
+if set -q FISH_BLOCK_CURSOR
+    set -gx fish_cursor_default block
+    set -gx fish_cursor_insert block blink
+    set -gx fish_cursor_external block blink
+    set -gx fish_cursor_replace_one underscore
+    set -gx fish_cursor_visual block
+else
+    set -gx fish_cursor_default block
+    set -gx fish_cursor_insert line blink
+    set -gx fish_cursor_external line blink
+    set -gx fish_cursor_replace_one underscore
+    set -gx fish_cursor_visual block
+end
 
 if status is-interactive
     if string match -q -r -- '.*ghostty.*|xterm-256color' $TERM
