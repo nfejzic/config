9999d61d6fc9321cebadae40f62423114b14302c
Nadir Fejzić <nadir@notfloor.com>
2025-09-04 13:32:36 +0200

    chore: simplify CLI theming

--- fish/user/init.fish
+++ fish/user/init.fish
@@ -18,11 +18,16 @@ functions --erase import
 # NO IMPORTS AFTER THIS LINE
 set -gx GPG_TTY (tty)
 
+# NOTE: - this is used externally
+# @fish-lsp-disable-next-line 4004
 function reload_config
     source ~/.config/fish/config.fish
 end
 
 starship init fish | source
+
+# NOTE: this disables fish greeting
+# @fish-lsp-disable-next-line 4004
 set fish_greeting
 
 if test -d $HOME/Developer/nix-template
