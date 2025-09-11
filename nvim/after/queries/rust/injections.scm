591b144b5e8dbc2ccb99e7632d7fb4e84eb0ae59
Nadir Fejzić <nadir@notfloor.com>
2025-09-11 14:35:45 +0200

    feat(nvim): add injections for doc comments code

--- nvim/after/queries/rust/injections.scm
+++ nvim/after/queries/rust/injections.scm
@@ -5,3 +5,14 @@
     (#match? @injection.content "^[ \t\n]*(CREATE|SELECT|INSERT|UPDATE|DELETE|WITH|USE|DROP|ALTER|SET).*")
     (#set! injection.language "sql")
 )
+
+(
+    (line_comment (doc_comment) @_first)
+    (line_comment (doc_comment) @injection.content)+
+    (line_comment (doc_comment) @_last)
+    (#match? @_first "```(rust|ignore.*|should_panic|no_run|compile_fail|edition.*)?$")
+    (#match? @_last "```$")
+    (#set! injection.language "rust")
+    (#set! injection.combined)
+)
+
