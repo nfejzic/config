211fd4a06b34061b19f7897d9d991f4b05492ecf
Nadir Fejzić <nadir@notfloor.com>
2025-09-28 14:42:01 +0200

    fix(nvim): disable rust doc comments injections
    This hinders treesitter performance by a lot in some cases, so let's
    disable it for now.

--- nvim/after/queries/rust/injections.scm
+++ nvim/after/queries/rust/injections.scm
@@ -6,13 +6,14 @@
     (#set! injection.language "sql")
 )
 
-(
-    (line_comment (doc_comment) @_first)
-    (line_comment (doc_comment) @injection.content)+
-    (line_comment (doc_comment) @_last)
-    (#match? @_first "```(rust|ignore.*|should_panic|no_run|compile_fail|edition.*)?$")
-    (#match? @_last "```$")
-    (#set! injection.language "rust")
-    (#set! injection.combined)
-)
-
+; NOTE: this makes treesitter very slow...
+; (
+;     (line_comment (doc_comment) @_first)
+;     (line_comment (doc_comment) @injection.content)+
+;     (line_comment (doc_comment) @_last)
+;     (#match? @_first "```(rust|ignore.*|should_panic|no_run|compile_fail|edition.*)?$")
+;     (#match? @_last "```$")
+;     (#set! injection.language "rust")
+;     (#set! injection.combined)
+; )
+;
