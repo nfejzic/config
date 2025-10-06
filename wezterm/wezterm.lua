6207cd38efc0b4db28efb1710837c454986b81df
Nadir Fejzić <nadir@notfloor.com>
2025-10-06 09:56:41 +0200

    chore(wezterm): add some type definitions

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -7,10 +7,17 @@ local config = wezterm.config_builder()
 
 local smart_splits = wezterm.plugin.require('https://github.com/mrjones2014/smart-splits.nvim')
 
+--- @class SmartSplits
+--- @field apply_to_config function(config_builder: Config, plugin_config: SmartSplitsWeztermConfig|nil): Config
+--- @field is_vim function(pane: Wezterm.Pane): boolean
+
 --- @class UserPlugins
 local plugins = {
+	--- @type SmartSplits
 	smart_splits = smart_splits,
 }
+
+---@alias IsVimFn function(pane: wezterm.Pane): boolean
 
 local library = require("lib.init")
 library.setup(wezterm, config, plugins)
