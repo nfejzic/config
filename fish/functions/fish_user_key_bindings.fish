031fa3b5620f782c975c6bbcc354c711e9ec79fe
Nadir Fejzić <nadir@notfloor.com>
2025-12-05 12:27:59 +0100

    fix(fish): disable Ctrl-D, used in CLI programs

--- fish/functions/fish_user_key_bindings.fish
+++ fish/functions/fish_user_key_bindings.fish
@@ -1,3 +1,9 @@
 function fish_user_key_bindings
-  fish_vi_key_bindings
+    fish_vi_key_bindings
+
+    # unbind Ctrl-D, it's useful for scrolling in TUI programs such as neovim:
+    bind -e --preset ctrl-d
+    bind -e --preset -M insert ctrl-d
+    bind -e --preset -M normal ctrl-d
+    bind -e --preset -M visual ctrl-d
 end
