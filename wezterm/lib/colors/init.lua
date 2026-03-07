a34c66d53f899b48150f616d8b960fcd7182c9cd
Nadir Fejzić <nadir@notfloor.com>
2026-03-07 15:29:25 +0100

    feat(wezterm): update colors

--- wezterm/lib/colors/init.lua
+++ wezterm/lib/colors/init.lua
@@ -6,6 +6,7 @@ local M = {}
 --- | "kanagawa-wave"
 --- | "kanagawa-lotus"
 --- | "gruvbox-dark-hard"
+--- | "solarized-dark-hard"
 -- NOTE: following themes are nice, but not yet configured
 -- --- | "Gruvbox light, medium (base16)"
 -- --- | "catppuccin-frappe"
@@ -31,13 +32,7 @@ end
 ---@param os_appearance OsAppearance
 ---@return string
 function M.get_color_scheme(theme, os_appearance)
-	if theme == "gruvbox-dark-hard" then
-		require("wezterm").color.load_scheme(
-		"~/Developer/config/wezterm/lib/colors/gruvbox-dark-hard.lua")
-	end
-
 	local theme_name = is_light_mode(os_appearance) and theme.light or theme.dark
-
 	return theme_name
 end
 
