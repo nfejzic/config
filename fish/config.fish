d5740ea225b23de266001469be80f47f26e870c9
Nadir Fejzić <nadir@notfloor.com>
2026-03-27 13:15:21 +0100

    feat(fish): improve fish_title

--- fish/config.fish
+++ fish/config.fish
@@ -3,31 +3,41 @@ builtin source "$HOME/.config/fish/user/init.fish"
 function gen_title
     set cmd_name $argv[1]
     set dir $argv[2]
-    set max_width 11
+    set max_width 15
 
-    # # we assume both current_command and current_dir are always available!
-    # # we have command + () + dir, must fit in max_width
     set occupied_len (math "$(string length $cmd_name) + 2")
     set space_for_dir (math "max($max_width - $occupied_len, 3)")
 
-    set dir (string shorten -c . -m $space_for_dir $dir)
+    if set -q argv[3]
+        set -l avail (math "$space_for_dir - 1")
+        set -l half (math "ceil($avail / 2)")
+        set dir (string shorten -c "" -m $half $dir):(string shorten -c '' -m (math "$avail - $half") $argv[3])
+    else
+        set dir (string shorten -c . -m $space_for_dir $dir)
+    end
 
     echo "$cmd_name($dir)"
 end
 
 function fish_title
-    # set -q argv[1]; or set argv fish
-    # # Looks like ~/Developer/config/fish: git log
-    # # or /etc/apt: fish
-    # echo (fish_prompt_pwd_full_dirs=3 prompt_pwd): $argv
+    set -l cmd (test -n "$argv[1]"; and echo $argv[1]; or echo fish)
+    set -l cmd (string split " " "$cmd")[1]
 
-    if test -e ./.git
-        # git repository, do it slightly differently
-        # NOTE: this might not work great, let's test for a while
-        set dir (git remote -vv | awk -F '/' '{print $NF}' | awk -F '.' '{print $1}' | head -n 1)
+    if git rev-parse --is-inside-work-tree &>/dev/null
+        set -l repo (git remote -vv 2>/dev/null \
+            | awk '{print $2}' \
+            | awk -F '/' '{print $NF}' \
+            | awk -F '.' '{print $1}' \
+            | head -n 1)
+
+        set -l gitdir (git rev-parse --git-dir 2>/dev/null)
+        set -l commondir (git rev-parse --git-common-dir 2>/dev/null)
+        if test (realpath "$gitdir") != (realpath "$commondir")
+            gen_title $cmd $repo $(git branch --show-current)
+        else
+            gen_title $cmd $repo
+        end
     else
-        set dir (basename "$(prompt_pwd)")
+        gen_title $cmd (basename "$(prompt_pwd)")
     end
-
-    gen_title $_ $dir
 end
