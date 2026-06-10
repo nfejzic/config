b01f4b3ba1da5a02778dfb89750346368c3b552b
Nadir Fejzic <nadir@notfloor.com>
2026-06-10 16:35:30 +0200

    feat(nvim): add custom highlights for rust

--- nvim/after/queries/rust/highlights.scm
+++ nvim/after/queries/rust/highlights.scm
@@ -1,7 +1,18 @@
 ; extends
-(("ref") @accent (#set! priority 190))
-(("unsafe") @accent (#set! priority 190))
-((mutable_specifier) @accent (#set! priority 190))
+(
+ ("unsafe") @accent
+ (#set! "priority" 199)
+)
+
+(
+ ("ref") @accent
+ (#set! "priority" 199)
+)
+
+(
+ (mutable_specifier) @accent
+ (#set! "priority" 199)
+)
 
 (outer_doc_comment_marker) @comment.documentation
 (inner_doc_comment_marker) @comment.documentation
